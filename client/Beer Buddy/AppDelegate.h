05ffb8e514ce4b2d036bef4ecdac9e203e099f99
Paul Wagener <mail@paulwagener.nl>
2012-07-21 10:20:31 +0200

    First Xcode project commit

---
+++ client/Beer Buddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Beer Buddy
+//
+//  Created by Paul Wagener on 21-07-12.
+//  Copyright (c) 2012 Paul Wagener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ client/Beer Buddy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Beer Buddy
+//
+//  Created by Paul Wagener on 21-07-12.
+//  Copyright (c) 2012 Paul Wagener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
